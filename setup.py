480a005c803bfd78f41ca52111b4f930f2139b21
icydoge <icydoge@gmail.com>
2016-10-02 17:31:31 +0100

    Allow user package mode for setup.

--- setup.py
+++ setup.py
@@ -13,10 +13,17 @@ from os import name
 def print_separator():
     print("-------------------------------------")
 
-def pip_install(packages):
+def pip_install(packages, install_as_user):
+    
     for package in packages:
+
         print("Langwith: installing dependency package " + str(package) + "...")
-        pip.main(['install', package])
+
+        if install_as_user:
+            pip.main(['install', '--user', package])
+        else:
+            pip.main(['install', package])
+
         print_separator()
 
 generic_dependencies = ['gevent', 'greenlet', 'pyopenssl', 'ndg-httpsclient', 'pyasn1', 'urllib3', 'requests']
@@ -26,14 +33,26 @@ if __name__ == '__main__':
 
     print("Langwith: Set up initiating...")
     print_separator()
+    
+    #Bind input in Python 2
+
+    try:
+        input = raw_input
+    except NameError:
+        pass
+
+    try:
+        install_as_user = False if str(input("Do you want to install packages for the system (with root/Administrator access) instead of just for your user (no root access)? Please enter 'y' if you are running this inside virtualenv. [y/N]:")).lower() == 'y' else True
+    except SyntaxError:
+        install_as_user = False 
 
     if name == "nt":
         print("Langwith: We seems to be on Windows, installing ported curses for command line graphics:")
-        pip_install(['vendor/curses-2.2-cp35-none-win32.whl']) #Curses for Windows
+        pip_install(['vendor/curses-2.2-cp35-none-win32.whl'], install_as_user) #Curses for Windows
     
     print_separator()
     print("Langwith: Installing dependencies for all platforms...")
-    pip_install(generic_dependencies)
+    pip_install(generic_dependencies, install_as_user)
     print("Langwith: Finished installing dependencies, if you see no error messages you can now execute 'python main.py' to start running Langwith.")
     
 else:
